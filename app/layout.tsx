b00babf23fca3772e2704e4e9ebf7a051104d732
Iago S. <me@iagosousa.com>
2024-08-26 22:02:08 -0300

    chore: back to simple webpage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
 import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Iago's Website",
   description: "My personal website",
 };
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
 
 export default function RootLayout({
   children,
@@ -14,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={GeistSans.className}>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,33 @@
-import { Header } from './components/header';
-import { Footer } from './components/footer';
-
 export default function Home() {
   return (
-    <div>
-      <Header />
-      <div className="w-full h-full py-24 bg-white flex flex-col items-center justify-start gap-16">
-        <div className="self-stretch h-[428px] px-8 flex flex-col items-center justify-start gap-16">
-          <div className="self-stretch flex justify-start items-start gap-16">
-            <div className="flex-1 flex flex-col justify-start items-start">
-              <div className="self-stretch h-[418px] flex flex-col justify-start items-start">
-                <div className="self-stretch text-[#475467] text-lg font-normal leading-7 font-inter break-words">
-                  <div className="font-bold mb-8">A little about me</div>
-                  <div className="mb-8">
-                    I'm a Brazilian-based DevSecOps engineer currently working at Lojas Renner, a prominent retail company. My role involves developing a developer portal, leveraging my expertise in cloud technologies and secure development practices. Previously, I served as an SRE at Renner, where I honed my skills in maintaining robust and scalable systems, earning my Certified Kubernetes Administrator (CKA) certification along the way.
-                  </div>
-                  <div className="mb-8">
-                    With a strong foundation in JavaScript, Python, Golang, and Rust, I bring a versatile skill set to the table. I excel in troubleshooting complex issues and am constantly pushing my boundaries to deepen my knowledge of system-level programming. My goal is to transition into international opportunities, applying my diverse experience and passion for low-level development to new challenges abroad.
-                  </div>
-                </div>
-                <div className="self-stretch h-[18px]"></div>
-              </div>
-            </div>
-            <div className="flex-1 flex flex-col justify-start items-start gap-12">
-              <div className="self-stretch h-[428px] flex flex-col justify-start items-start">
-                <div className="self-stretch text-[#475467] text-lg font-normal leading-7 font-inter break-words">
-                  Sagittis et eu at elementum, quis in. Proin praesent volutpat egestas sociis sit lorem nunc nunc sit. Eget diam curabitur mi ac. Auctor rutrum lacus malesuada massa ornare et. Vulputate consectetur ac ultrices at diam dui eget fringilla tincidunt. Arcu sit dignissim massa erat cursus vulputate gravida id. Sed quis auctor vulputate hac elementum gravida cursus dis.<br />
-                  Lectus id duis vitae porttitor enim gravida morbi.<br />
-                  Eu turpis posuere semper feugiat volutpat elit, ultrices suspendisse. Auctor vel in vitae placerat.<br />
-                  Suspendisse maecenas ac donec scelerisque diam sed est duis purus.<br />
-                  Ipsum sit mattis nulla quam nulla. Gravida id gravida ac enim mauris id. Non pellentesque congue eget consectetur turpis. Sapien, dictum molestie sem tempor.
-                </div>
-              </div>
-            </div>
-          </div>
+    <div className="flex flex-col w-full h-screen bg-white">
+      <div className="flex-1 flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-4xl leading-loose font-bold">
+            Iago S.
+          </h1>
+          <p className="text-2xl">DevSecOps Engineer</p>
+          <p className="w-1/2 text-justify">
+            Com quatro anos de experiência em desenvolvimento de software, sou um desenvolvedor curioso e apaixonado por tecnologia.
+            Atualmente trabalhando como DevSecOps Engineer, também tive experiência com SRE, onde adquiri um bom conhecimento em Kubernetes e fui certificado como CKA (Certified Kubernetes Administrator).
+            Minha expertise está em desenvolvimento web e automação. Atualmente estudando Go, Rust e Zig, busco cada vez mais entrar na área de engenharia de sistemas, desenvolvendo ferramentas que ajudem o time a entregar software mais rápido e com mais qualidade.
+          </p>
         </div>
       </div>
-      <Footer />
+      <footer className="flex justify-end p-4">
+        <div className="flex space-x-4">
+          <a href="https://github.com/iagosrodrigues" target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-gray-800">
+            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+              <path fillRule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clipRule="evenodd" />
+            </svg>
+          </a>
+          <a href="https://linkedin.com/in/iagosr" target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-gray-800">
+            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+              <path fillRule="evenodd" d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" clipRule="evenodd" />
+            </svg>
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
